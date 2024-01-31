018b90d9a0d9f409b82cf8fed1e11dea5d4bbecf
Luzvelia <luzvelia.ene@gmail.com>
2024-01-31 09:15:25 +0100

    actualizacion tercera

--- script.js
+++ script.js
@@ -3,25 +3,19 @@ function crearForm() {
     var formulario = document.createElement("form");
     var mostrar = document.createElement("div");
 
-
     var tituloName = document.createElement("label");
     var inputName = document.createElement("input");
 
     var tituloApellido = document.createElement("label");
     var inputApellido = document.createElement("input");
 
-
     var tituloDni = document.createElement("label");
     var inputDni = document.createElement("input");
-
 
     var tituloEmail = document.createElement("label");
     var inputEmail = document.createElement("input");
 
     var button = document.createElement("button");
-
-    // 
-
 
     // añadir inputs y label al formulario
     formulario.appendChild(tituloName);
@@ -35,7 +29,7 @@ function crearForm() {
   
     formulario.appendChild(tituloEmail);
     formulario.appendChild(inputEmail);
-
+    
     formulario.appendChild(button);
 
     // Agregar texto al label
@@ -45,15 +39,11 @@ function crearForm() {
     tituloEmail.innerHTML ='<h3>Email</h3>';
     button.innerHTML ='<h3>Validar</h3>';
 
-
-
     // Crear textos para el div mostrar
     var textName = document.createElement("p");
     var textApellido = document.createElement("p");
     var textDni = document.createElement("p");
     var textEmail = document.createElement("p");
-
-
 
      // añadir textos al div mostrar
      mostrar.appendChild(textName);
@@ -61,105 +51,48 @@ function crearForm() {
      mostrar.appendChild(textDni);
      mostrar.appendChild(textEmail);
  
-
-
-
     // Agregar atributos a los inputs
+    formulario.setAttribute('style', "width:25%; margin:auto; display:flex; flex-direction:column; justify-content:center;")
+
+    mostrar.setAttribute('style', "width:25%; margin:auto; display:flex; flex-direction:column; justify-content:center;")
 
     inputName.setAttribute('type', "text");
     inputName.setAttribute('placeholder', "Nombre");
     inputName.setAttribute('id', "inputName");
+    inputName.setAttribute('style', "box-shadow: #0000003d 0px 3px 4px; padding:5px; border-radius:7px; border-color:#d0d0f1;");    
     textName.setAttribute('id', "textName");
 
-    
     inputApellido.setAttribute('type', "text");
     inputApellido.setAttribute('placeholder', "Apellido");
     inputApellido.setAttribute('id', "inputApellido");
+    inputApellido.setAttribute('style', "box-shadow: #0000003d 0px 3px 4px; padding:5px; border-radius:7px; border-color:#e3e3f0;");
     textApellido.setAttribute('id', "textApellido");
     
     inputDni.setAttribute('type', "text");
     inputDni.setAttribute('placeholder', "DNI");
     inputDni.setAttribute('id', "inputDni");
+    inputDni.setAttribute('style', "box-shadow: #0000003d 0px 3px 4px; padding:5px; border-radius:7px; border-color:#e3e3f0;");
     textDni.setAttribute('id', "textDni");
 
     
     inputEmail.setAttribute('type', "email");
     inputEmail.setAttribute('placeholder', "Email");
     inputEmail.setAttribute('id', "inputEmail");
+    inputEmail.setAttribute('style', "box-shadow: #0000003d 0px 3px 5px; margin-bottom:10px; padding:5px; border-radius:7px; border-color:#e3e3f0;");
     textEmail.setAttribute('id', "textEmail");
 
 
     button.setAttribute('type', "button");
-    button.setAttribute('style', "width:10%;margin: 20px;padding: 5px;border-radius: 30%; background:#8d8df3;color:#fff;border:none 1px #000;");
     button.setAttribute('id', "button");
-
+    button.setAttribute('style', "box-shadow:#0000003d 0px 3px 8px;; color:white; width:80px; height:50px; margin:auto; padding:2px; border-radius:15px; border:none;background-color:#8d8df3;");
 
     document.body.appendChild(formulario);
     document.body.appendChild(mostrar);
-    console.log(formulario);
-    console.log(mostrar);
 }
 
 crearForm();    
 
-
-// Validar campos vacíos
-button.addEventListener("click", ()=> {
-    if(inputName.value.length == ""){
-        document.getElementById("textName").innerHTML="El campo Nombre está vacío, por favor ingrese el dato solicitado";
-    }
-    if(inputApellido.value.length == ""){
-        document.getElementById("textApellido").innerHTML="El campo Apellido está vacío, por favor ingrese el dato solicitado";
-
-    }
-    if(inputDni.value.length == ""){
-        document.getElementById("textDni").innerHTML="El campo DNI está vacío, por favor ingrese el dato solicitado";
-
-    }
-    if(inputEmail.value.length == ""){
-        document.getElementById("textEmail").innerHTML="El campo Email está vacío, por favor ingrese el dato solicitado";
-
-    }
-}) 
-
-
-
-function validarNombre() {
-    var nombre = document.getElementById("inputName").value;
-    var contador  = 0;
-
-    for (let i = 0; i < nombre.length; i++) {
-        if(esLetra(nombre[i])){
-            contador++;
-        } 
-    }
-    console.log(contador);
-    return contador == nombre.length;
-
-    
-}
-
-
-
-function validarApellido() {
-    var apellido = document.getElementById("inputApellido").value;
-    var contador  = 0;
-
-   
-
-    for (let i = 0; i < apellido.length; i++) {
-        if(esLetra(apellido[i])){
-            contador++;
-        } else{
-            
-        }
-    }
-    console.log(contador);
-    return contador == apellido.length;
-}
-
-
-
+// funcion de las letras con ASCII
 
 function esLetra(c) { 
     return c.charCodeAt() >= 'a'.charCodeAt() && c.charCodeAt() <= 'z'.charCodeAt() || c.charCodeAt() == ' '.charCodeAt() 
@@ -167,26 +100,70 @@ function esLetra(c) {
     
 }
 
+// Validacion de nombre
+function validarNombre() {
+    var nombre = document.getElementById("inputName").value;
+    var contador  = 0;
 
+    for (let i = 0; i < nombre.length; i++) {
+        if(esLetra(nombre[i])){
+            contador++;
+            textName.style.color="blue";
+        }else{
+            textName.style.color="red";
+        }  
+    }
+    return contador == nombre.length;
+}
+
+// Validacion de apellido
+
+function validarApellido() {
+    var apellido = document.getElementById("inputApellido").value;
+    var contador  = 0;
+
+    for (let i = 0; i < apellido.length; i++) {
+        if(esLetra(apellido[i])){
+            contador++;
+            textApellido.style.color="blue";
+        }else{
+            textApellido.style.color="red";
+        }
+    }
+    return contador == apellido.length;
+}
+
+
+// mostrar los textos de los inputs
 button.addEventListener("click", ()=>{
-    
     textName.innerHTML = document.getElementById("inputName").value;
     textApellido.innerHTML = document.getElementById("inputApellido").value;
     textDni.innerHTML = document.getElementById("inputDni").value;
     textEmail.innerHTML = document.getElementById("inputEmail").value;
-
-
-
-    if (textName.innerHTML === true){
-        textName.innerHTML.setAttribute('style', "color:blue;")
-
-    } else{
-        textName.innerHTML.setAttribute('style', "color:red;")
-
+    validarNombre();
+    validarApellido();
+    validarDni();
     }
-
-
-}
 )
 
+// Validar campos vacíos
+button.addEventListener("click", ()=> {
+    if(inputName.value === ""){
+        textName.style.color="red";
+        textName.innerHTML="Campo vacío, ingrese los datos";
+    }
+    if(inputApellido.value === ""){
+        textApellido.style.color="red";
+        textApellido.innerHTML="Campo vacío, ingrese los datos";
 
+    }
+    if(inputDni.value === ""){
+        textDni.style.color="red";
+        textDni.innerHTML="Campo vacío, ingrese los datos";
+
+    }
+    if(inputEmail.value === ""){
+        textEmail.style.color="red";
+        textEmail.innerHTML="Campo vacío, ingrese los datos";   
+    }
+}) 
